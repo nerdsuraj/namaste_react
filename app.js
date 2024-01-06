ee556382506006a9b5849f4cffa1cfda66e961be
nerdsuraj <soorajkrpandit@gmail.com>
2024-01-06 12:22:52 +0530

    parcel and tut push!

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 // const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, React.createElement('h1', { }, "this is call child and parent!!!")));
 
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, [React.createElement('h1', {}, "this is h1 tag !"),React.createElement('h2', {}, "this is h2 tag!!!")]));
 
 
