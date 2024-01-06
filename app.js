6baf1d8410d96c3b31ef87c86c3dc84ebb5fe816
nerdsuraj <soorajkrpandit@gmail.com>
2024-01-06 15:39:25 +0530

    third tut push!!

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // const heading = React.createElement('h1', {id:"head"}, "Namaste React from using the react!!!!");
 
-
-
 // const parent = React.createElement('div', { id: "parent" }, React.createElement('div', { id: "child" }, React.createElement('h1', { }, "this is call child and parent!!!")));
 
 
@@ -13,6 +11,34 @@ const parent = React.createElement('div', { id: "parent" }, React.createElement(
 
 console.log("🚀 ~ file: app.js:4 ~ parent:", parent) //its a object
 
+//jsx code transform into javascript code by ---> parcel ---> babel
+
+// JSX ==> React.createElement  ==> React.createElement_JS Object ==> HTMLElement(render)
+const jsxheading =  (<h1 id="head">Namaste React from using the react!!!!  </h1> )
+
+//create the react function component
+const Heading = () => (<h1 id="head">Namaste React from using the react!!!! {elem} </h1>  )
+
+const elem = <span>hello</span>
+const numbbers = 10000;
+
+const HrChild = () => ( 
+    <div>
+        <h1 id="head">Namaste React from using child!!!</h1>
+        <h3>{elem}</h3>
+        <h2>{numbbers}</h2>
+        <Heading />
+        <Heading ></Heading>
+        {Heading()}
+    </div>
+)
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+// root.render(jsxheading);
+
+
+//if we render it
+root.render(<HrChild/>);
